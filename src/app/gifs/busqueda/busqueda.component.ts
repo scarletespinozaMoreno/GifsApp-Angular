527ac9b2047c6fca894783d7c6bd491ec23fdb1d
Scarlet Angelina Espinoza Moreno <scanespi@espol.edu.ec>
2021-10-01 22:24:03 -0500

    viewChild -obtener referencias a objetos del HTML

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,ElementRef,ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
@@ -6,11 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BusquedaComponent implements OnInit {
+export class BusquedaComponent  {
 
-  constructor() { }
+  // ! operadora para asegurarse que el objeto no es nulo
+  //  ViewChild va a byscar en el html el elemento que tenga un referencia local llamada txtbyscar
+  @ViewChild('txtBuscar') txtBuscar!:ElementRef <HTMLInputElement>;
 
-  ngOnInit(): void {
-  }
+     buscar(){
+        const valor = this.txtBuscar.nativeElement.value;
+        this.txtBuscar.nativeElement.value='';
+     }
 
 }
